c81eea7ca49eb2a0758467551c3dffae2224df9f
Mary Jenel Myers <mmyers7@hawaii.edu>
2015-07-11 17:24:20 -0700

    added Search Item Object Class

---
+++ Crossroads/SearchItem.h
@@ -0,0 +1,19 @@
+//
+//  SearchItem.h
+//  Crossroads
+//
+//  Created by Mary Jenel Myers on 7/11/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface SearchItem : NSObject
+
+@property NSString *itemName;
+@property UIImage *itemImage;
+
+-(instancetype)initWithName: (NSString *)itemName itemImage:(UIImage *)itemImage;
+
+@end
