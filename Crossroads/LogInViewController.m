e1bf7a294eb0f9ee0c5dc69a6f1d5f77c3e8e150
mewpre <yichin.sun@gmail.com>
2015-07-11 22:05:58 -0700

    Added login/signup views

---
+++ Crossroads/LogInViewController.m
@@ -0,0 +1,43 @@
+//
+//  LogInViewController.m
+//  Crossroads
+//
+//  Created by Yi-Chin Sun on 7/11/15.
+//  Copyright (c) 2015 Mary Jenel Myers. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)onLoginButtonTapped:(id)sender {
+}
+- (IBAction)onSignUpButtonTapped:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Crossroads/SignUpViewController.m
@@ -0,0 +1,44 @@
+//
+//  SignUpViewController.m
+//  Crossroads
+//
+//  Created by Yi-Chin Sun on 7/11/15.
+//  Copyright (c) 2015 Mary Jenel Myers. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *emailTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (strong, nonatomic) IBOutlet UITextField *verifyPasswordTextField;
+@property (strong, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)onSignUpButtonTapped:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
